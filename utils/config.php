9959228f1f323c9eaa880574ff41a5ee8119876e
nigel <nigelnkambule32@gmail.com>
2026-04-29 06:56:53 +0200

    feat(utils): implement automatic database configuration

---
+++ utils/config.php
@@ -0,0 +1,29 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'javax@Nk');
+define('DB_NAME', 'village_bank');
+
+// will connect to MySQL server only first, since this is the initial setup, we dont have our actual village_bank database
+function get_server_db() {
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS);
+
+    if ($conn->connect_error) {
+        die("Server connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+
+// then connect to village_bank database
+function get_db() {
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if ($conn->connect_error) {
+        die("Database connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+?>
