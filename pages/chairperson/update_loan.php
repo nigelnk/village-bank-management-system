ac5c08cbddca38e90be72d642dd71cf77bc6484b
nigel <nigelnkambule32@gmail.com>
2026-04-30 05:59:26 +0200

    feat(loan_management): implement core loan management functionality
    Add icons for the cards to improve UI.
    
    Add helper update_loan.php and give_loan.php files for better organization.

---
+++ pages/chairperson/update_loan.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../../utils/config.php';
+
+$conn = get_db();
+
+$id = $_POST['loan_id'];
+$paid = $_POST['amount_paid'];
+$interest = $_POST['interest'];
+
+$conn->query("
+UPDATE loans 
+SET total_paid = total_paid + $paid,
+balance = balance - $paid,
+interest = $interest
+WHERE id = $id
+");
+
+header("Location: loan_management.php");
