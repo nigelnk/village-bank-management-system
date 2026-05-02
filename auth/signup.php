7801268e84304092a1f73cd29a5bd18272e7e183
fortunesalijeni-dot <anniesalijeni@gmail.com>
2026-05-03 00:34:12 +0200

    Modified the sign up page

--- auth/signup.php
+++ auth/signup.php
@@ -2,88 +2,55 @@
 <html lang="en">
 
 <head>
-    <title>Sign Up</title>
-<link href="../../static/css/addmember.css" rel="stylesheet">
+    <title>Village Bank Sign Up</title>
+
+    <link rel="stylesheet" href="../static/css/signin.css">
+
 </head>
 
 <body>
-    
-    <h2>Join Our Group</h2>
 
     <div class="container">
-        <div class="sidebar">
-            <h2>Village Bank</h2>
-            <ul>
-            <li> <a href="#">Dashboard</a></li>
-            <li> <a href="#">Dashboard</a></li>
-            <li> <a href="#">Dashboard</a></li>
-            <li> <a href="#">Dashboard</a></li>
-            <li> <a href="#">Dashboard</a></li>
-            <li> <a href="#">Dashboard</a></li>
-            <li> <a href="#">Dashboard</a></li>
-                
-            </ul>
-             </div>
-             
-<form method="POST" action="">
-   
-    <div class="main-content">
 
-         <!--Personal Details-->
-    <h4>Personal Details</h4>
-    <div class="form-row">
-<div class="form-group">
-    <label for="fname">First Name:</label>
-    <input type="text" name="fname" required>
-    </div>
-    <div class="form-group">
-    <label for="lname">Last Name:</label>
-    <input type="text" name="lname" required>
-    </div>
-    <div class="form-group">
-        <div class="gender-group">
-    <label for="gender">Gender:</label> 
-    <label for="female">Female</label>
-    <input type="radio" name="gender">
-    <label for="male">Male</label>
-    <input type="radio" name="gender">
-</div>
-    </div>
-    <div class="form-group">
-    <label for="id">National ID Number:</label>
-    <input type="text" name="id">
-</div>
-</div>
+        <div class="title">
+            <h3>WELCOME TO NANSADI VILLAGE BANK</h3>
+        </div>
 
-    <!--Contact Information-->
-    <h4>Contact Details</h4>
-    <div class="form-row">
-    <label for="phonenumber">Phone Number:</label>
-    <input type="text" name="phonenumber">
-    <label for="address">Physical Address:</label>
-    <textarea  name="address" id="address"></textarea>
-</div>
-    <!--Next of Kin-->
-    <h4>Emergency Contact</h4>
-    <div class="form-row">
-    <label for="fullname">Full Name:</label>
-    <input type="text" name="fullname">
-    <label for="relationship">Relationship to member:</label>
-    <select name="relationship" id="relationship">
-        <option value="sibling">Sibling</option>
-        <option value="parent">Parent</option>
-        <option value="spouse">Spouse</option>
-        <option value="other">Other</option>
-    </select>
-    <label for="pnumber">Phone Number:</label>
-    <input type="text" name="pnumber">
-</div>
+        <div class="subtitle">
+            <img src="../../static/photos/logo.png" alt="Village Bank Logo">
+        </div>
 
-<div class="buttons">
-<button type="submit" class="save" name="save">Save Member</button>
-<button type="reset" class="reset" name="reset">Reset</button>
-</div> </div> </div>
-</form>
+        <div class="form">
+
+            <form method="post" action="log_in_page.php">
+
+                <div class="username">
+                    <input type="text" name="username" required placeholder="username">
+                </div>
+
+                <div class="pass">
+                    <input type="password" name="password" required placeholder="Enter Password">
+                </div>
+
+                <div class="sign up">
+                    <input type="submit" value="Sign Up">
+                </div>
+
+            </form>
+
+            <div class="support">
+
+
+                <div class="create">
+                    <button type="button">Create New Account</button>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
 </body>
 
 </html>
