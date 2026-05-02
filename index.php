d4592e8604a74e716594ced681443d3abecb576b
nigel <nigelnkambule32@gmail.com>
2026-05-02 12:53:42 +0200

    feat(index): make index the entry page
     create database if it does not exists otherwise will redirect to login page

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+require_once './utils/config.php';
+
+$mysqli = get_server_db();
+$dbname = "village_bank";
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+// check if the database exists
+$db_exists = $mysqli->query("SHOW DATABASES LIKE '$dbname'");
+
+if ($db_exists && $db_exists->num_rows > 0) {
+    // database exists, redirect to login page
+    header("Location: ../auth/login.php");
+    exit();
+} else {
+    // if database does not exist, run setup
+    require_once 'utils/db_setup.php';
+}
+
+$mysqli->close();
